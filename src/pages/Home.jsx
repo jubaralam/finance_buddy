c0f818fe6bf2eb6e523b19ea7917c581aae9d098
Zubair Alam <zubairalam@Zubairs-MacBook-Air.local>
2025-04-12 19:37:24 +0530

    footer is completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,22 @@ import HeroSection from "../components/HeroSection";
 import RealTimeProjection from "../components/RealTimeProjection";
 import { AnnualFinancialOverviewChart } from "../components/AnnualFinancialOverviewChart";
 import { financialProjectionData } from "../data";
+import Footer from "../components/Footer";
 
 const Home = () => {
-  
   return (
     <div className="bg-color h-screen">
-      <Navbar />
-      <HeroSection />
-      <RealTimeProjection />
-      <AnnualFinancialOverviewChart data={financialProjectionData} />
+      <header>
+        <Navbar />
+        <HeroSection />
+      </header>
+      <main>
+        <RealTimeProjection />
+        <AnnualFinancialOverviewChart data={financialProjectionData} />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
